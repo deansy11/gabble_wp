ebab478e24c47ce4659ff8b5df438c74b9edd925
Emily Deans <emilyldeans@gmail.com>
2017-08-11 14:17:08 -0400

    Established corresponding models and migrations.

--- models/gab.js
+++ models/gab.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(sequelize, DataTypes) {
-  var gab = sequelize.define('gab', {
+  let gab = sequelize.define('gab', {
     description: DataTypes.STRING
   }, {
     classMethods: {
